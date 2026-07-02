a02b08e3ead37fbae1c680c82f5baf6c33a2806b
Rvsolution007 <antigravity97732@gmail.com>
2026-07-02 12:31:32 +0530

    Fix: Stack overflow infinite loop in trFormat string extension

--- brandkit_mobile/lib/utils/string_extensions.dart
+++ brandkit_mobile/lib/utils/string_extensions.dart
@@ -3,7 +3,7 @@ import 'package:get/get.dart';
 
 extension StringFormatting on String {
   String get trFormat {
-    String translated = this.trFormat;
+    String translated = this.tr;
     if (translated == this) {
       String spaced = replaceAll('_', ' ');
       if (spaced.isEmpty) return spaced;
